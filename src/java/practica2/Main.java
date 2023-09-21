938725f7a7bafe62e58b1f05c4f650032bdef36f
Leonardo Gallo <lgallo@ciencias.unam.mx>
2023-09-21 01:10:58 -0600

    se agrega práctica 2

---
+++ src/java/practica2/Main.java
@@ -0,0 +1,38 @@
+package icc.practica02;
+
+/**
+ * Clase con el método principal
+ *
+ * @author 
+ * @version 1
+ *
+/
+class main {
+
+/*
+* Método principal
+* @param args
+*/
+public estatic void main(String []args){
+
+//Declaración de variables
+String mensaje = "Corrigiendo código";
+char a;
+int i = 1; a = 'b';
+// valores por defecto en tipos primitivos
+byte octeto = 0;
+short corto = 0;
+int entero = 0;
+long largo = 0;
+float flotante = 0;
+double doble = 0;
+char caracter = "a";
+boolean bool = true;
+
+System.out.printnl("mensaje"); //Linea que imprime el valor de la variable mensaje
+System.out.printnl("El valor de i es " + 1);
+System.out.printnl("El caracter almacenado es " + a);
+
+System.out.printnl("Mostramos los valores por defecto en cada tipo de dato primitivo"):
+
+}
