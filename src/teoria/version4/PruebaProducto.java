9d0fa56f2ff202940f9572028ed4067f355e2e9e
Leonardo Gallo <lgallo@ciencias.unam.mx>
2023-09-21 15:20:56 -0600

    versión 4 de teoria para completar por el alumno

--- src/teoria/version4/PruebaProducto.java
+++ src/teoria/version4/PruebaProducto.java
@@ -1,1 +1,15 @@
-// Completa la prueba de las clases del paquete teoria.version3
+package teoria.version4;
+
+/**
+ *
+ * Clase de prueba de un producto
+ * en la versión 4, usando constructores.
+ */
+public class PruebaProducto {
+
+    // Coméntame...
+    public static void main(String[] args) {
+
+    }
+
+}
