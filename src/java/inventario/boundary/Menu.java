bb326fc5c79ef080659985388e53b3326906f199
Leonardo Gallo <lgallo@ciencias.unam.mx>
2023-11-14 15:38:56 -0600

    Se incluye la plantilla del menú

---
+++ src/java/inventario/boundary/Menu.java
@@ -0,0 +1,26 @@
+package ui; //paquete al que pertenece la clase
+
+//importación de otros paquetes
+import java.util.Scanner;
+import utilidades.Funcion;
+
+/**
+ * COMÉNTAME ...
+ * ¿Para qué sirve la clase?
+ * Propósito de la clase
+ */
+public class Menu {
+
+    private String[] opciones;
+    private String instrucciones;
+
+    public Menu(String[] opciones) {
+
+
+    }
+
+    @Override
+    public String toString() {
+	return "";
+    }
+}
